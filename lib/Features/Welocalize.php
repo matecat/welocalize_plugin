d2bd5a5feb03296d4f285698a5349630451948c2
Vincenzo Ruffa <vincenzoruffa1991@gmail.com>
2018-05-22 16:01:51 +0200

    callback for autoload

--- lib/Features/Welocalize.php
+++ lib/Features/Welocalize.php
@@ -11,6 +11,7 @@ namespace Features;
 
 use Features\Welocalize\Model\Analysis\CustomPayableRates;
 use Klein\Klein;
+use BasicFeatureStruct;
 
 class Welocalize extends BaseFeature {
 
@@ -21,6 +22,25 @@ class Welocalize extends BaseFeature {
 
     public function filterPayableRates( $payableRates, $SourceLang, $TargetLang ){
         return CustomPayableRates::getPayableRates( $SourceLang, $TargetLang );
+    }
+
+    /**
+     * As Autoload Plugin Welocalize set itself as project feature when an user is logged
+     *
+     * @param $projectFeatures
+     * @param $__postInput
+     * @param $userIsLogged
+     *
+     * @return array
+     */
+    public function filterCreateProjectFeatures( $projectFeatures, $__postInput, $userIsLogged ) {
+
+        if( $userIsLogged ){
+            $projectFeatures[ self::FEATURE_CODE ] = new BasicFeatureStruct( [ 'feature_code' => self::FEATURE_CODE ] );
+        }
+
+        return $projectFeatures;
+
     }
 
 }
